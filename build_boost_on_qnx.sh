a6dbc8dbd5d76a03c3efadf5f370cb7b686b1aea
GW00253738 <wangke@noboauto.com>
2022-03-29 14:14:48 +0800

    fix the target directory

--- build_boost_on_qnx.sh
+++ build_boost_on_qnx.sh
@@ -4,4 +4,4 @@ wget -c https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1
   tar -xf boost_1_78_0.tar.bz2 && \
   patch boost_1_78_0/boost/interprocess/permissions.hpp permissions.hpp.patch && \
   cd boost_1_78_0 && ./bootstrap.sh && \
-  ./b2 --user-config=../qnx.jam --without-python target-os=qnx link=shared install --prefix=$QNX_TARGET/usr
+  ./b2 --user-config=../qnx.jam --without-python target-os=qnx link=shared install --prefix=$QNX_TARGET/aarch64le/usr
