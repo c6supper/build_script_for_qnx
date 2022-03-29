d929c718c05724f4d5aa54b997668f6998fdd00f
GW00253738 <wangke@noboauto.com>
2022-03-29 14:03:24 +0800

    fix the boot

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # boost_qnx
+## Fix the compile problem manually , refer to [Fix](https://github.com/boostorg/config/issues/288)
+
+## Run ./build_boost_on_qnx.sh
+```bash
+  
+```
--- build_boost_on_qnx.sh
+++ build_boost_on_qnx.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-wget -c https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.tar.gz && \
-  tar -xf boost_1_78_0.tar.gz && \
+wget -c https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.tar.bz2 && \
+  tar -xf boost_1_78_0.tar.bz2 && \
   patch boost_1_78_0/boost/interprocess/permissions.hpp permissions.hpp.patch && \
   cd boost_1_78_0 && ./bootstrap.sh && \
-  ./b2 --user-config=../qnx.jam --without-python target-os=qnx link=shared install --prefix=$QNX_TARGET/user
+  ./b2 --user-config=../qnx.jam --without-python target-os=qnx link=shared install --prefix=$QNX_TARGET/usr
